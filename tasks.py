f3d89796df0d03b20e2bdb1af45a629cc6cdca46
Jaakko Airikkala <jaakkel@gmail.com>
2025-01-20 18:55:23 +0200

    Add tasks

---
+++ tasks.py
@@ -0,0 +1,16 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
