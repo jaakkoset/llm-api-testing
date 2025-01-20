81d20c9e560d66fb6d99cf540a2ef759007cb857
Jaakko Airikkala <jaakkel@gmail.com>
2025-01-20 19:51:53 +0200

    Add coverage

--- tasks.py
+++ tasks.py
@@ -14,3 +14,13 @@ def test(ctx):
 @task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest; coverage html", pty=True)
+
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src; coverage report -m", pty=True)
