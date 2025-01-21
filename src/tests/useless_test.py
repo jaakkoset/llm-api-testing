f1cb225b88c86893292ef9700bb4a6ce94e85250
Jaakko Airikkala <jaakkel@gmail.com>
2025-01-21 13:22:03 +0200

    Diff hit should pass but overall coverage fall

--- src/tests/useless_test.py
+++ src/tests/useless_test.py
@@ -10,3 +10,20 @@ class TestUseless(unittest.TestCase):
         text = self.useless.multiply_text(5, "a")
         should_be = "aaaaa"
         self.assertEqual(should_be, text)
+
+    def test_first_letter(self):
+        first_letter = self.useless.first_letter("apple")
+        should_be = "a"
+        self.assertEqual(should_be, first_letter)
+
+        first_letter = self.useless.first_letter("banana")
+        should_be = "b"
+        self.assertEqual(should_be, first_letter)
+
+        first_letter = self.useless.first_letter("Apple")
+        should_be = "A"
+        self.assertEqual(should_be, first_letter)
+
+        first_letter = self.useless.first_letter("Öylätti")
+        should_be = "Ö"
+        self.assertEqual(should_be, first_letter)
--- src/useless.py
+++ src/useless.py
@@ -9,3 +9,20 @@ class Useless:
 
     def multiply_text(self, factor: int, text: str) -> str:
         return factor * text
+
+    def first_letter(self, text: str) -> str:
+        first_letter = None
+        if not isinstance(text, str):
+            print("not good")
+            return "Text must be a string"
+
+        if text[0] == "a":
+            first_letter = "a"
+        elif text[0] == "b":
+            first_letter = "b"
+        elif text[0] == "A":
+            first_letter = "A"
+        else:
+            first_letter = text[0]
+
+        return first_letter
