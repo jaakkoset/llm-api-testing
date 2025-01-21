3e80a8ffc9c2ee8642dc60c4b08c1b9ac06c765f
Jaakko Airikkala <jaakkel@gmail.com>
2025-01-21 11:23:36 +0200

    Edit coverage tasks

--- tasks.py
+++ tasks.py
@@ -18,9 +18,6 @@ def lint(ctx):
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest; coverage html", pty=True)
-
-
-@task
-def coverage_report(ctx):
-    ctx.run("coverage run --branch -m pytest src; coverage report -m", pty=True)
+    ctx.run(
+        "coverage run --branch -m pytest; coverage report -m;coverage html", pty=True
+    )
