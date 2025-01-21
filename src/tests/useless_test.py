763cac7feac00a6285e639e1fa7a5cbaa7da05a0
Jaakko Airikkala <jaakkel@gmail.com>
2025-01-21 16:08:58 +0200

    Diff hit over 70, overall coverage falls

--- src/tests/useless_test.py
+++ src/tests/useless_test.py
@@ -6,24 +6,23 @@ class TestUseless(unittest.TestCase):
     def setUp(self):
         self.useless = Useless()
 
+    def test_useless(self):
+        value = self.useless.useless(21)
+        self.assertEqual("BIG", value)
+
     def test_multiply_text(self):
         text = self.useless.multiply_text(5, "a")
-        should_be = "aaaaa"
-        self.assertEqual(should_be, text)
+        self.assertEqual("aaaaa", text)
 
     def test_first_letter(self):
         first_letter = self.useless.first_letter("apple")
-        should_be = "a"
-        self.assertEqual(should_be, first_letter)
+        self.assertEqual("a", first_letter)
 
         first_letter = self.useless.first_letter("banana")
-        should_be = "b"
-        self.assertEqual(should_be, first_letter)
+        self.assertEqual("b", first_letter)
 
         first_letter = self.useless.first_letter("Apple")
-        should_be = "A"
-        self.assertEqual(should_be, first_letter)
+        self.assertEqual("A", first_letter)
 
         first_letter = self.useless.first_letter("Öylätti")
-        should_be = "Ö"
-        self.assertEqual(should_be, first_letter)
+        self.assertEqual("Ö", first_letter)
--- src/useless.py
+++ src/useless.py
@@ -2,10 +2,20 @@ class Useless:
     def __init__(self):
         self.text = "Useless text"
 
-    # def useless(self, value: int):
-    #     if value > 20:
-    #         return "BIG"
-    #     return "smoll"
+    def useless(self, value: int):
+        if value > 20:
+            print("BIG")
+            print("BIG")
+            print("BIG")
+            print("BIG")
+            print("BIG")
+            print("BIG")
+            print("BIG")
+            print("BIG")
+            return "BIG"
+        print("smoll")
+        print("smoll")
+        return "smoll"
 
     def multiply_text(self, factor: int, text: str) -> str:
         return factor * text
