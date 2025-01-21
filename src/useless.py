7539990f1559e5579452702704628739b622a6a2
Jaakko Airikkala <jaakkel@gmail.com>
2025-01-21 12:13:07 +0200

    Test that Codecov fails, because new code is not tested

--- src/useless.py
+++ src/useless.py
@@ -1,3 +1,8 @@
 class Useless:
     def __init__(self):
         self.text = "Useless text"
+
+    def useless(self, value: int):
+        if value > 20:
+            return "BIG"
+        return "smoll"
