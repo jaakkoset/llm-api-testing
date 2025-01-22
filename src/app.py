c73f44b7344acebdcce43b2d7351634e8b2514d4
Jaakko Airikkala <jaakkel@gmail.com>
2025-01-22 18:22:58 +0200

    Add method for checking if given company name is valid

--- src/app.py
+++ src/app.py
@@ -7,5 +7,21 @@ class LLM:
 
     def simulate_customers(self, company) -> str:
         """Returns descriptions for three simulated customers for the given company"""
+        company_is_valid = self.company_is_valid(company)
+        print(company_is_valid)
         prompt = f"simulate three future customer profiles for {company}"
         return self.llm.get_response(prompt)
+
+    def company_is_valid(self, company) -> bool:
+        """Asks Gemini if it is comfortable with creating user profiles for the given
+        company. Raises ValueError if the answer is unclear.
+
+        Returns
+        -------
+            True or False"""
+        prompt = (
+            f"Would you say you have enough information about the company {company} "
+            f"to create simulated user profiles for it? Please answer only YES or NO"
+        )
+        response = self.llm.get_response(prompt)
+        return response
