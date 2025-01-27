7cdb21026dce7d78a89b9be77a355572acc02c66
Jaakko Airikkala <jaakkel@gmail.com>
2025-01-27 12:21:04 +0200

    Program UI loops

--- src/index.py
+++ src/index.py
@@ -1,8 +1,9 @@
 from app import Simulator
 
-company = input("Name of company you want to study: ")
-print("\nSimulating three customers...\n")
-simulator = Simulator()
+while True:
+    company = input("Name of company you want to study: ")
+    print("\nSimulating three customers...\n")
+    simulator = Simulator()
 
-response = simulator.simulate_customers(company)
-print(response)
+    response = simulator.simulate_customers(company)
+    print(response)
